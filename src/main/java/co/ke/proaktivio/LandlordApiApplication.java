95a57c520f8e35364eb5f6e3d332b2722c442cbc
Kevin Mbugua <METROPOL0\kevin.mbugua@kitktwa14l010.metropol.local>
2019-10-04 16:11:56 +0300

    enable web flux

--- src/main/java/co/ke/proaktivio/LandlordApiApplication.java
+++ src/main/java/co/ke/proaktivio/LandlordApiApplication.java
@@ -2,8 +2,10 @@ package co.ke.proaktivio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class LandlordApiApplication {
 
 	public static void main(String[] args) {
